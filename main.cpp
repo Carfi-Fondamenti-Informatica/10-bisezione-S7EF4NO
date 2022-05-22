82e9479c25b673f8d0ddeeaadc3929d6e72ffcf2
S7EF4NO <100076214+S7EF4NO@users.noreply.github.com>
2022-05-22 21:21:19 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <iostream>
+#include "cmath"
+#include "iomanip"
+
 using namespace std;
 
+float funzione(float c)
+{
+    float y;
+    y = (c*c * cos(c)) + 1;
+    return y;
+}
+
 int main() {
-   return 0;
+
+    float a, b,err,x;
+    do {
+        cout << "inserire estremi" << endl;
+        cin >> a >> b;
+    } while (funzione(a) * funzione(b) >= 0);
+
+    do {
+        x = (a + b) / 2;
+        if (funzione(x) == 0) {
+            cout<<int(x*10000.0)/10000.0<<endl;
+            //return 0;
+        } else {
+            if (funzione(a) * funzione(x) < 0) {
+                b = x;
+            } else {
+                a = x;
+            }
+
+             err = abs((b - a) / 2);
+
+        }
+    } while(err>=1e-6);
+    cout<<int(x*10000.0)/10000.0<<endl;
+    return 0;
 }
